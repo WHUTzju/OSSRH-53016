a9df98672ad6bb3b7a7f2a73dc846d9fb09fd7d8
zhangrui <zhangrui@hyperchain.cn>
2019-11-16 09:29:19 +0800

    init

---
+++ src/main/java/com/whut/Ossrh53016Application.java
@@ -0,0 +1,13 @@
+package com.whut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ossrh53016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ossrh53016Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/whut/Ossrh53016ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ossrh53016ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
